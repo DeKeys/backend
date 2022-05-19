8c73caba94dc4584ea442c87945ab6053278c618
SkullMag <rybalko.oleg.123@mail.ru>
2022-05-19 11:59:41 +0300

    Main file for running application

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import auth 
+from fastapi.logger import logger
+import logging
+import os
+import data.db_session as db_session
+
+
+db_session.global_init(os.path.join("db", "db.sqlite3"))
+logger.setLevel(logging.DEBUG)
+
+app = FastAPI()
+app.include_router(auth.router)
