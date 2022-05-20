440db20c04906c7cc8b31bb9107c95e376f21a78
SkullMag <rybalko.oleg.123@mail.ru>
2022-05-20 12:45:03 +0300

    Added function for signature verification

--- routers/auth.py
+++ routers/auth.py
@@ -15,40 +15,44 @@ from typing import List
 router = APIRouter(prefix="/api")
 
 
-@router.post("/initUser/", status_code=status.HTTP_200_OK)
-def init_user(user: InitUserModel, response: Response):
-    session = db_session.create_session()
-    # Load sent public key
+def verify_signature(model):
+    # Load public key
     pub_key = serialization.load_pem_public_key(
-        unhexlify(user.public_key)
+        unhexlify(model.public_key)
     )
-
+    # Check verification string length
+    if len(model.verification_string) != 256:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return "Verification string length should be 256"
     try:
-        if len(user.verification_string) != 256:
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return "Verification string length should be 128 bytes"
         # Verify signature
         pub_key.verify(
-            unhexlify(user.signature),
-            unhexlify(user.verification_string),
+            unhexlify(model.signature),
+            unhexlify(model.verification_string),
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA512()),
                 salt_length=padding.PSS.MAX_LENGTH
             ),
             hashes.SHA512()
         )
-        
-        # Find user in database
-        if db_user := session.query(User).where(User.public_key == user.public_key).first():
-            passwords: List[Password] = session.query(Password).where(Password.user_id == db_user.id).all()
-            # Get passwords from ipfs and return
-        else:
-            # Create new user
-            new_user = User()
-            new_user.public_key = user.public_key
-            session.add(new_user)
-            session.commit()
-            return "{}"
+        return True
     except InvalidSignature:
+        return False
+
+
+@router.post("/init_user/", status_code=status.HTTP_200_OK)
+def init_user(user: InitUserModel, response: Response):
+    session = db_session.create_session()
+    verification_check = verify_signature(user)
+
+    if verification_check is False:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return "Invalid signature"
+        
+    # Find user in database
+    if (db_user := session.query(User).where(User.public_key == user.public_key).first()) is None:
+        # Create new user
+        new_user = User()
+        new_user.public_key = user.public_key
+        session.add(new_user)
+        session.commit()
