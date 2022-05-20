2c460878198c2a4733bc4d11695f29c3da05ec24
SkullMag <rybalko.oleg.123@mail.ru>
2022-05-20 11:55:11 +0300

    Passwords model for database

---
+++ data/passwords.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+from .db_session import SqlAlchemyBase
+
+
+class Password(SqlAlchemyBase):
+    __tablename__ = "passwords"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    service = Column(String)
+    password_address = Column(String)
+
