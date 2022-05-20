8257b732c6f2f015ae199967ebc14790f7d423c0
SkullMag <rybalko.oleg.123@mail.ru>
2022-05-20 13:17:22 +0300

    Created function for adding passwords

--- routers/passwords.py
+++ routers/passwords.py
@@ -1,10 +1,53 @@
 from fastapi import APIRouter, Response, status
 from models.password import Password
+from models.errors import ErrorTypes
+from data import db_session
+from data.users import User
+import data.passwords
+from routers.auth import verify_signature
+import requests
+import json
+from hashlib import sha512
 
 
 router = APIRouter(prefix="/api")
 
 
 @router.post("/add_password", status_code=status.HTTP_200_OK)
-def add_password(password: Password):
-    print(password)
+def add_password(password: Password, response: Response):
+    # Verify signature
+    session = db_session.create_session()
+    if verification_check := verify_signature(password):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return verification_check
+
+    # Check if user exists
+    user = session.query(User).where(User.public_key == password.public_key).first()
+    if user is None:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return ErrorTypes.ACCOUNT_NOT_EXISTS
+
+    # Add password to IPFS
+    data = {
+        "service": password.service,
+        "login": password.login,
+        "password": password.password
+    }
+
+    resp = requests.post("http://127.0.0.1:5001/api/v0/add", files={
+        sha512(password.service + password.login + password.password + password.public_key).hexdigest(): json.dumps(data)
+    })
+    if resp.status_code != 200:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return ErrorTypes.FAILED_ADD_PASSWORD_TO_IPFS
+    addr = resp.json()["Hash"]
+
+    # Add password to database
+    pwd = data.passwords.Password()
+    pwd.user_id = user.id
+    pwd.address = addr
+    session.add(pwd)
+    session.commit()
+
+    return addr
+    
