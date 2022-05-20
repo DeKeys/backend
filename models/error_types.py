6f89201288ba5eed5b2cba3bb25a7d517116172c
Roman Rakhlin <rrakhlin@gmail.com>
2022-05-20 14:20:17 +0300

    renamed errors.py to error_types.py

--- models/error_types.py
+++ models/error_types.py
@@ -7,4 +7,4 @@ class ErrorTypes:
     ACCOUNT_NOT_EXISTS = "You should create account"
     INVALID_VERIFICATION_STRING_LENGTH = "Verification string length should be 256"
     FAILED_ADD_PASSWORD_TO_IPFS = "Failed to add password to IPFS"
-
+    PASSWORD_NOT_EXIST = "Password doesn't exist"
