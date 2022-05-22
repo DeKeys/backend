4ad2116d6208736f5b600fb74451f63601efcdc7
Roman Rakhlin <rrakhlin@gmail.com>
2022-05-22 21:59:19 +0300

    add new tests into script for running tests

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,16 @@
 from api_tests.add_password_test import AddPasswordTests
 from api_tests.get_passwords_test import GetPasswordsTest
+from api_tests.edit_passwords_test import EditPasswordsTest
+from api_tests.delete_passwords_test import DeletePasswordsTest
 import unittest
 
 
 tests = unittest.TestSuite()
+
 tests.addTest(unittest.makeSuite(AddPasswordTests))
 tests.addTest(unittest.makeSuite(GetPasswordsTest))
+tests.addTest(unittest.makeSuite(EditPasswordsTest))
+tests.addTest(unittest.makeSuite(DeletePasswordsTest))
+
 unittest.TextTestRunner().run(tests)
+
