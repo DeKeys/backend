7c155669615c6111510ae5849a5ae19b970a188a
SkullMag <rybalko.oleg.123@mail.ru>
2022-05-23 14:02:24 +0300

    Created tests for user init and fixed typos

--- run_tests.py
+++ run_tests.py
@@ -1,12 +1,14 @@
+from api_tests.init_user_test import InitUserTests
 from api_tests.add_password_test import AddPasswordTests
 from api_tests.get_passwords_test import GetPasswordsTest
-from api_tests.edit_passwords_test import EditPasswordsTest
-from api_tests.delete_passwords_test import DeletePasswordsTest
+from api_tests.edit_password_test import EditPasswordsTest
+from api_tests.delete_password_test import DeletePasswordsTest
 import unittest
 
 
 tests = unittest.TestSuite()
 
+tests.addTest(unittest.makeSuite(InitUserTests))
 tests.addTest(unittest.makeSuite(AddPasswordTests))
 tests.addTest(unittest.makeSuite(GetPasswordsTest))
 tests.addTest(unittest.makeSuite(EditPasswordsTest))
