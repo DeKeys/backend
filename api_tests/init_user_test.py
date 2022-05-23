240c6b8c244b8b04d07be0744ddd3b1dad26a3ff
SkullMag <rybalko.oleg.123@mail.ru>
2022-05-23 14:07:30 +0300

    Added tests for invalid signature and data overflow

--- api_tests/init_user_test.py
+++ api_tests/init_user_test.py
@@ -7,4 +7,9 @@ import unittest
 class InitUserTests(unittest.TestCase):
     def test_successful_init(self):
         self.assertEqual(init_user(PUB_KEY_STRING, SIGNATURE, DATA), True)
-        
+    
+    def test_invalid_signature(self):
+        self.assertEqual(init_user(PUB_KEY_STRING, SIGNATURE + b"a", DATA), False) 
+
+    def test_data_overflow(self):
+        self.assertEqual(init_user(PUB_KEY_STRING, SIGNATURE, DATA + b"a"), False)
