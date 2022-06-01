dc87010512a0a38fd227f7b553774f9ef783f257
Roman Rakhlin <rrakhlin@gmail.com>
2022-06-01 14:09:09 +0300

    add documentation to data folder

--- data/db_session.py
+++ data/db_session.py
@@ -4,12 +4,14 @@ from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
 from sqlalchemy.pool import StaticPool
 
-SqlAlchemyBase = dec.declarative_base()
 
+SqlAlchemyBase = dec.declarative_base()
 __factory = None
 
 
 def global_init(db_file):
+    """Initialization of the main functionality."""
+
     global __factory
 
     if __factory:
@@ -18,7 +20,7 @@ def global_init(db_file):
     if not db_file or not db_file.strip():
         raise Exception("Необходимо указать файл базы данных.")
 
-    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    conn_str = f"sqlite:///{db_file.strip()}?check_same_thread=False"
 
     engine = sa.create_engine(conn_str, poolclass=StaticPool, echo=False)
     __factory = orm.sessionmaker(bind=engine)
@@ -29,5 +31,8 @@ def global_init(db_file):
 
 
 def create_session() -> Session:
+    """Creation of a new session."""
+
     global __factory
     return __factory()
+
